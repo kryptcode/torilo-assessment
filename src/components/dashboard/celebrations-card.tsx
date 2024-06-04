3899a28f3b1eec758705716844f2a61dc91d78b7
Koye <54333700+kryptcode@users.noreply.github.com>
2024-06-04 16:28:17 +0100

    ""

--- src/components/dashboard/celebrations-card.tsx
+++ src/components/dashboard/celebrations-card.tsx
@@ -14,13 +14,13 @@ export const CelebrationsCard = () => {
   return (
     <div className="bg-white shadow-md border p-3  rounded-lg">
       <h4 className="text-xl font-medium mb-2">Celebrations</h4>
-      <div className="flex mb-4 bg-[#f2f2f2] border p-1.5 px-3 text-sm items-center font-medium justify-between rounded-xl ">
+      <div className="flex mb-4 bg-[#f2f2f2] border p-1.5 px-2 text-xs items-center font-medium justify-between rounded-xl ">
         <div
           className={`${
             activeTab === "birthdays"
               ? "bg-white text-black font-semibold"
               : "text-[#878787]"
-          } p-1.5 px-3 rounded-lg cursor-pointer transition-all ease-in-out duration-500`}
+          } p-1.5 px-1.5 rounded-lg cursor-pointer transition-all ease-in-out duration-500`}
           onClick={() => setActiveTab("birthdays")}
         >
           Birthdays
@@ -30,7 +30,7 @@ export const CelebrationsCard = () => {
             activeTab === "ani"
               ? "bg-white text-black font-semibold"
               : "text-[#878787]"
-          } p-1.5 px-3 rounded-lg cursor-pointer transition-all ease-in-out duration-500`}
+          } p-1.5 px-1.5 rounded-lg cursor-pointer transition-all ease-in-out duration-500`}
           onClick={() => setActiveTab("ani")}
         >
           Job Anniversary
@@ -40,7 +40,7 @@ export const CelebrationsCard = () => {
             activeTab === "new"
               ? "bg-white text-black font-semibold"
               : "text-[#878787]"
-          } p-1.5 px-3 rounded-lg cursor-pointer transition-all ease-in-out duration-500`}
+          } p-1.5 px-1.5 rounded-lg cursor-pointer transition-all ease-in-out duration-500`}
           onClick={() => setActiveTab("new")}
         >
           New Hire
